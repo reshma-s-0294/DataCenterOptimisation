46eba4a2dcd3cdff8becd3ff191bed9de1359e4b
Rohitbadgujar <rohit.badgujar2009@gmail.com>
2019-03-21 21:00:33 +0000

    db test case

--- src/com/ucc/dc/tests/DBManagerTest.java
+++ src/com/ucc/dc/tests/DBManagerTest.java
@@ -2,13 +2,24 @@ package com.ucc.dc.tests;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+
 import org.junit.Test;
 
+import com.ucc.dc.dao.DBManager;
+/**
+ * Test to DB connection
+ * 
+ * by
+ * Rohit Badgujar
+ */
 public class DBManagerTest {
 
 	@Test
 	public void testGetConnection() {
-		fail("Not yet implemented");
+		DBManager dbManager = new DBManager();
+		Connection connection = dbManager.getConnection();
+		assertTrue(connection != null);
 	}
 
 }
