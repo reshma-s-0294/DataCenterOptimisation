7969ed416065ac3fd88e4acccb80c6f3b6cc721a
Rohitbadgujar <rohit.badgujar2009@gmail.com>
2019-03-21 20:56:52 +0000

    Test case to check db connection

---
+++ src/com/ucc/dc/tests/DBManagerTest.java
@@ -0,0 +1,14 @@
+package com.ucc.dc.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBManagerTest {
+
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+}
