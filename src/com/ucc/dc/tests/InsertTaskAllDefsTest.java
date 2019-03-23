c152e8304b53dee32ae872929fddfed521661c8d
Rohitbadgujar <rohit.badgujar2009@gmail.com>
2019-03-23 02:14:40 +0000

    added test cases

--- src/com/ucc/dc/tests/InsertTaskAllDefsTest.java
+++ src/com/ucc/dc/tests/InsertTaskAllDefsTest.java
@@ -27,5 +27,58 @@ public class InsertTaskAllDefsTest  extends Mockito {
         
 		assertTrue(expected == response);
 	}
+	
+	@Test
+	public void testValidateInputsForPath2_1() {
+
+        boolean expected = true;
+        
+        HttpServletRequest request = mock(HttpServletRequest.class);
+		when(request.getParameter("type")).thenReturn("web");
+        when(request.getParameter("deadline")).thenReturn("2000");
+        
+        boolean response = new InsertTask().validateInputs(request);
+        
+		assertTrue(expected == response);
+	}
+	@Test
+	public void testValidateInputsForPath2_2() {
+
+        boolean expected = false;
+        
+        HttpServletRequest request = mock(HttpServletRequest.class);
+		when(request.getParameter("type")).thenReturn("web");
+        when(request.getParameter("deadline")).thenReturn("test");
+        
+        boolean response = new InsertTask().validateInputs(request);
+        
+		assertTrue(expected == response);
+	}
+	@Test
+	public void testValidateInputsForPath2_3() {
+
+        boolean expected = false;
+        
+        HttpServletRequest request = mock(HttpServletRequest.class);
+		when(request.getParameter("type")).thenReturn("web");
+        when(request.getParameter("deadline")).thenReturn("400");
+        
+        boolean response = new InsertTask().validateInputs(request);
+        
+		assertTrue(expected == response);
+	}
+	@Test
+	public void testValidateInputsForPath2_4() {
+
+        boolean expected = true;
+        
+        HttpServletRequest request = mock(HttpServletRequest.class);
+		when(request.getParameter("type")).thenReturn("web");
+        when(request.getParameter("deadline")).thenReturn("1050");
+        
+        boolean response = new InsertTask().validateInputs(request);
+        
+		assertTrue(expected == response);
+	}
 
 }
